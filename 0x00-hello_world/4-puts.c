a703b356f053b15a13862de00eb00c2f933cc51c
samuel <samaremu65@gmail.com>
2022-03-11 07:28:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,10 @@
  *
  * main - main block
  *
- * Return: 0
+ * Return:Always 0
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
 }
