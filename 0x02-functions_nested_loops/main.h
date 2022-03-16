4ecccb8f15b596d59786a58b6551f71edbf5f8ab
samuel <samaremu65@gmail.com>
2022-03-15 18:13:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int r);
-int print_last_digit(int j);
-int _abs(int r);
-int print_last_digit(int j);
+void jack_bauer(void);
