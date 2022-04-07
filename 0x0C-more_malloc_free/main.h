00000327ba24dfdea17743f52964385e477d1fe3
Samthlen <99646935+Samthlen@users.noreply.github.com>
2022-04-08 07:42:38 +0800

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,34 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
-char *_strcat(char *, char *);
-char *_strncat(char *, char *, int);
-char *_strncpy(char *, char *, int);
-int _strcmp(char *, char *);
-void reverse_array(int *, int);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int);
-char *infinite_add(char *, char *, char *, int);
-void print_buffer(char *, int);
-
-char *create_array(unsigned int, char);
-char *_strdup(char *);
-char *str_concat(char *, char *);
-int **alloc_grid(int, int);
-void free_grid(int **, int);
-char *argstostr(int, char **);
-char **strtow(char *);
-
-void *malloc_checked(unsigned int);
-char *string_nconcat(char *, char *, unsigned int);
-void *_calloc(unsigned int, unsigned int);
-int *array_range(int, int);
-void *_realloc(void *, unsigned int, unsigned int);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
-
-
