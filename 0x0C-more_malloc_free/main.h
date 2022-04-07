25898340c7074548204516b758e659e13615d05e
Samthlen <99646935+Samthlen@users.noreply.github.com>
2022-04-08 07:28:25 +0800

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int);
+char *infinite_add(char *, char *, char *, int);
+void print_buffer(char *, int);
+
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+char **strtow(char *);
+
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
 
 #endif
+
 
